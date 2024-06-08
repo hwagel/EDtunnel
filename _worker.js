6768b064caceebfca9d8dcc6978fb4d391489623
hwagel <107284052+hwagel@users.noreply.github.com>
2024-06-08 09:18:30 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'eff85c47-bceb-46c2-8573-d8918edeb265';
 
-const พร็อกซีไอพีs = ['139.162.60.187'];
+const พร็อกซีไอพีs = ['15.235.163.231'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
